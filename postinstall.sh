cb43c966b472f53aa4fcfecf3a268f6e616f57ba
Lars Kringelbach <lkr@trifork.com>
2017-06-21 16:23:15 +0200

    Postinstall fixed

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,7 @@ cd ios/Frameworks/ThePerfectApp.framework
 
 echo "Removing current Symlinks files"
 rm -f Headers
-rm -f Resources
+rm -f Modules
 rm -f ThePerfectApp
 rm -f Versions/Current
 	
@@ -15,5 +15,5 @@ ln -s A Current
 cd ..
 
 ln -s ./Versions/Current/Headers Headers
-ln -s ./Versions/Current/Resources Resources
+ln -s ./Versions/Current/Modules Modules
 ln -s ./Versions/Current/ThePerfectApp ThePerfectApp
